a3f15813633cbf8a80668c88e321f65d484909bb
Arthur CHERAMY <arthur.cheramy@gmail.com>
2020-07-29 14:22:45 +0200

    Ajout Grid (Etape 17)

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
@@ -24,23 +25,28 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			
-			Text text = new Text();
-			text.setText("Hello, world !");
-			text.setFont(Font.font("Cambria", FontPosture.ITALIC, 45));
-			root.setCenter(text);
+			Scene scene = new Scene(root,600,600);
 			
 			MenuBar menuBar = new MenuBar();
 			menuBar.getMenus().add(new Menu("Premier menu"));
 			menuBar.getMenus().add(new Menu("Deuxième menu"));
 			root.setTop(menuBar);
 			
+
+			GridPane grid = new GridPane();
+			
+			Text text = new Text();
+			text.setText("Hello, world !");
+			text.setFont(Font.font("Cambria", FontPosture.ITALIC, 45));
+			grid.add(text, 0, 1);			
+			
 			Rectangle rect2 = new Rectangle(60, 60, 200, 200);
             rect2.setFill(new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE, new Stop[] { new Stop(0, Color.BLACK), new Stop(1, Color.RED)}));
             rect2.setStroke(Color.GREEN);
             rect2.setStrokeWidth(10);
-            root.setBottom(rect2);
+            grid.add(rect2, 1,1);
+            
+            root.setCenter(grid);
 			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
